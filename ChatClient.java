c818838151a70cef4a78888f525dc80711fe7ab1
Marco Gonçalves <marco99goncalves@gmail.com>
2022-12-13 19:51:01 +0000

    Revert "m"
    This reverts commit 75ab1fe1a826fefcebc3603bd03a2c148e9adf28.

--- ChatClient.java
+++ ChatClient.java
@@ -3,10 +3,9 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.Toolkit;
-import java.net.Socket;
 
 
-public class ChatClient implements Runnable {
+public class ChatClient {
 
     // Variáveis relacionadas com a interface gráfica --- * NÃO MODIFICAR *
     JFrame frame = new JFrame("Discordamos");
@@ -17,15 +16,14 @@ public class ChatClient implements Runnable {
     // Se for necessário adicionar variáveis ao objecto ChatClient, devem
     // ser colocadas aqui
 
+
+
+
     // Método a usar para acrescentar uma string à caixa de texto
     // * NÃO MODIFICAR *
     public void printMessage(final String message) {
         chatArea.append(message);
     }
-
-    Socket connSocket;
-    DataOutputStream outToServer;
-    BufferedReader inFromServer;
 
 
     // Construtor
@@ -39,7 +37,7 @@ public class ChatClient implements Runnable {
         frame.setLayout(new BorderLayout());
         frame.add(panel, BorderLayout.SOUTH);
         frame.add(new JScrollPane(chatArea), BorderLayout.CENTER);
-        frame.setSize(1000, 1000);
+        frame.setSize(500, 300);
         frame.setVisible(true);
 
         java.net.URL url = ClassLoader.getSystemResource("Resources/icon.png");
@@ -53,8 +51,7 @@ public class ChatClient implements Runnable {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    String message = chatBox.getText();
-                    newMessage(message);
+                    newMessage(chatBox.getText() + "\n");
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 } finally {
@@ -72,10 +69,8 @@ public class ChatClient implements Runnable {
         // Se for necessário adicionar código de inicialização ao
         // construtor, deve ser colocado aqui
 
-        // Initialize the TCP Connection
-        connSocket = new Socket(server, port);
-        outToServer = new DataOutputStream(connSocket.getOutputStream());
-        inFromServer = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
+
+
     }
 
 
@@ -83,38 +78,18 @@ public class ChatClient implements Runnable {
     // na caixa de entrada
     public void newMessage(String message) throws IOException {
         // PREENCHER AQUI com código que envia a mensagem ao servidor
-        if (message == null || message.isEmpty())
-            return;
-
-        try {
-            outToServer.writeBytes(message + '\n');
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+        printMessage(message);
     }
+
 
     // Método principal do objecto
-    public void run() {
+    public void run() throws IOException {
         // PREENCHER AQUI
-        String res = null;
-        while(true){
-            try {
-                if (((res = inFromServer.readLine()) != null)){
-                    ServerResponse serverResponse = new ServerResponse(res);
 
-                    if(serverResponse.getMessage() != null)
-                        printMessage(serverResponse.toString() + "\n");
-                }
-            } catch (IOException e) {
-                System.out.println("ERRO: " + res);
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        }
 
 
     }
+
 
     // Instancia o ChatClient e arranca-o invocando o seu método run()
     // * NÃO MODIFICAR *
